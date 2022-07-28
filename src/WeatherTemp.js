9aab0fca77ce586b9db77235127e22c47c8f6e19
Zoia-K <evahekate@yahoo.com>
2022-07-28 13:39:11 +0300

    fahrenheit link activated

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function WeatherTemp(props) {
-  return (
-    <div className='WeatherTemp'>
-      <span className='main-temp mx-2'>{Math.round(props.celsius)}°</span>{' '}
-      <span className='main-units'>C|F</span>
-    </div>
-  );
+  const [unit, setUnit] = useState('celsius');
+
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit('fahrenheit');
+  }
+
+  if (unit === 'celsius') {
+    return (
+      <div className='WeatherTemp'>
+        <span className='main-temp mx-2'>{Math.round(props.celsius)}°</span>{' '}
+        <span className='main-units'>
+          C|{' '}
+          <a href='/' onClick={convertToFahrenheit}>
+            F
+          </a>{' '}
+        </span>
+      </div>
+    );
+  } else {
+    return 'F';
+  }
 }
